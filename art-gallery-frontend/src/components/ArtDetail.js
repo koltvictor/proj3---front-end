912624b7729232b8e55de47a479a15431dca2b05
+ <+>
2021-10-05 16:05:19 -0700

    added some details to ArtDetails

--- art-gallery-frontend/src/components/ArtDetail.js
+++ art-gallery-frontend/src/components/ArtDetail.js
@@ -15,8 +15,13 @@ function ArtDetail () {
 
     return(
         <div>
-            <img src={artShowing.image} alt={artShowing.title} />
+            <img height="500" width="500" src={artShowing.image} alt={artShowing.title} />
             <h2>{artShowing.artist}</h2>
+            <h2>Title: {artShowing.title}</h2>
+            <h3>Medium: {artShowing.medium}</h3>
+            <h3>Date of Creation: {artShowing.creation_date}</h3>
+            <h3>Number: {artShowing.number_of}</h3>
+
         </div>
     )
 }
