5e0c47b135ae7de4d60817b66315eceb44b797f0
+ <+>
2021-10-05 07:23:42 -0700

    fixed fetching

--- art-gallery-frontend/src/App.js
+++ art-gallery-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 //component imports
@@ -8,6 +8,15 @@ import MyArt from './components/MyArt'
 
 function App() {
   const [searchArt, setSearchArt] = useState('')
+  const [arts, setArts] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:9292')
+    .then((r) => r.json())
+    .then(setArts)
+  }, [])
+
+  console.log(arts)
 
   function handleSearch(e) {
     setSearchArt(e.target.value);
